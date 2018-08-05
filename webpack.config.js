fed8f26069f6ece500d6c245d4fdce7f8c9b1436
andefine <1939866551@qq.com>
2018-08-05 20:48:41 +0800

    Revert "Using webpack-dev-middleware"
    This reverts commit cc270d1a5874960578e3f37fb5a88968a0eb4aa3.

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,6 @@ module.exports = {
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    path: path.resolve(__dirname, 'dist')
   }
 };
